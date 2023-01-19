b64cd968d565228027d54a7a305eefea5b5ee29c
Žiga Deisinger <ziga@deisinger.si>
2023-01-19 14:22:10 +0100

    New: Fetch from Java

--- src/main/java/si/deisinger/Main.java
+++ src/main/java/si/deisinger/Main.java
@@ -12,8 +12,11 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		CheckForNewStationsProcessor checkForNewStationsProcessor = new CheckForNewStationsProcessor();
-		checkForNewStationsProcessor.checkPetrol();
-		checkForNewStationsProcessor.checkGremoNaElektriko();
+		switch (args[0]) {
+			case "gne" -> checkForNewStationsProcessor.checkGremoNaElektriko();
+			case "petrol" -> checkForNewStationsProcessor.checkPetrol();
+			default -> System.out.println("Invalid input.");
+		}
 	}
 }
 

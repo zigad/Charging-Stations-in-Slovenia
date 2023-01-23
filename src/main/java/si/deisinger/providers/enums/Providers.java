8515d3d5621b454bf7959529da44489dcedec7e8
Žiga Deisinger <ziga@deisinger.si>
2023-01-23 17:02:39 +0100

    New provider eFrend #18

--- src/main/java/si/deisinger/providers/enums/Providers.java
+++ src/main/java/si/deisinger/providers/enums/Providers.java
@@ -4,7 +4,6 @@ public enum Providers {
 	AVANT2GO("Avant2Go", "https://api.avant2go.com/api/locations?providerID=58ee0cc36d818563a9ff46af&populate=%5B%22companyID%22,%22providerID%22,%22regionID%22%5D&filters=%7B%22chargers%22%3A%5B1%5D%7D&limit=1000&position=14.815333%2C46.119944&searchFields=name%2Caddress.city%2Caddress.address1", null),
 	EFREND("eFrend", "https://efrend.eu.charge.ampeco.tech/api/v2/app/pins", "https://efrend.eu.charge.ampeco.tech/api/v2/app/locations"),
 	GREMO_NA_ELEKTRIKO("GremoNaElektriko", "https://cp.emobility.gremonaelektriko.si/api/v2/app/pins", "https://cp.emobility.gremonaelektriko.si/api/v2/app/locations"),
-	MOL_PLUNGEE("MOLPlungee", "", null),
 	MOON_CHARGE("MoonCharge", "https://charge.moon-power.si/DuskyWebApi//noauthlocations?UserGPSaccessLatitude=46.119944&UserGPSaccessLongitude=14.815333&searchRadius=200000&showAlsoRoaming=false", null),
 	PETROL("Petrol", "https://onecharge.eu/DuskyWebApi//noauthlocations?UserGPSaccessLatitude=46.119944&UserGPSaccessLongitude=14.815333&searchRadius=200000&showAlsoRoaming=false", null);
 

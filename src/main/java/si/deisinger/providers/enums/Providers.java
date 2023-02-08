f55905cc1f76f33689d019beb18eb672ba8268c2
Žiga Deisinger <ziga@deisinger.si>
2023-02-08 10:49:31 +0100

    Fix: Update MoonCharge URL #36

--- src/main/java/si/deisinger/providers/enums/Providers.java
+++ src/main/java/si/deisinger/providers/enums/Providers.java
@@ -3,7 +3,7 @@ package si.deisinger.providers.enums;
 public enum Providers {
 	GREMO_NA_ELEKTRIKO("GremoNaElektriko", "https://cp.emobility.gremonaelektriko.si/api/v2/app/pins", "https://cp.emobility.gremonaelektriko.si/api/v2/app/locations"),
 	PETROL("Petrol", "https://onecharge.eu/DuskyWebApi//noauthlocations?UserGPSaccessLatitude=46.119944&UserGPSaccessLongitude=14.815333&searchRadius=200000&showAlsoRoaming=false"),
-	MOON_CHARGE("MoonCharge", "https://charge.moon-power.si/DuskyWebApi//noauthlocations?UserGPSaccessLatitude=46.119944&UserGPSaccessLongitude=14.815333&searchRadius=200000&showAlsoRoaming=false"),
+	MOON_CHARGE("MoonCharge", "https://charge.moon-power.si/DuskyWebApi/api/locations?searchLatitude=46.119944&searchLongitude=14.815333&searchRadius=500&showAlsoRoaming=false&onlyCurrentlyAvailable=false&onlyFreeOfCharge=false"),
 	AVANT2GO("Avant2Go",
 			"https://api.avant2go.com/api/locations?providerID=58ee0cc36d818563a9ff46af&populate=%5B%22companyID%22,%22providerID%22,%22regionID%22%5D&filters=%7B%22chargers%22%3A%5B1%5D%7D&limit=1000&position=14.815333%2C46.119944&searchFields=name%2Caddress.city%2Caddress.address1"),
 	EFREND("eFrend", "https://efrend.eu.charge.ampeco.tech/api/v2/app/pins", "https://efrend.eu.charge.ampeco.tech/api/v2/app/locations"),

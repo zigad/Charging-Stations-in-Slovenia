7725f4276ca88f4c2d9fbfecad106b9219f99443
Žiga Deisinger <41201503+zigad@users.noreply.github.com>
2023-02-08 11:38:30 +0100

    Revert "Prepare master (#41)" (#45)
    This reverts commit 67e5a72aeb53ac1c8140419efae69a43cb2931de.

---
+++ src/main/java/si/deisinger/Main.java
@@ -0,0 +1,35 @@
+package si.deisinger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import si.deisinger.business.ProviderProcessor;
+import si.deisinger.providers.enums.Providers;
+
+public class Main {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+	/**
+	 * The main method of the class. This method invokes methods for checking each provider from the enum {@link Providers}. The provider is checked by calling the corresponding method from the {@link ProviderProcessor} class. Information about the provider
+	 * being checked is logged.
+	 *
+	 * @param args
+	 * 		the command line arguments, not used in this method.
+	 */
+	public static void main(String[] args) {
+		ProviderProcessor providerProcessor = new ProviderProcessor();
+		for (Providers provider : Providers.values()) {
+			LOG.info("Checking provider: " + provider.getProviderName());
+			switch (provider) {
+				case GREMO_NA_ELEKTRIKO -> providerProcessor.checkGremoNaElektriko(provider);
+				case PETROL -> providerProcessor.checkPetrol(provider);
+				case MOON_CHARGE -> providerProcessor.checkMoonCharge(provider);
+				case AVANT2GO -> providerProcessor.checkAvant2Go(provider);
+				case EFREND -> providerProcessor.checkEFrend(provider);
+				case IMPLERA -> providerProcessor.checkImplera(provider);
+				default -> throw new IllegalStateException("Unexpected value: " + provider);
+			}
+		}
+	}
+}
+
---
+++ src/main/java/si/deisinger/business/controller/EmailController.java
@@ -0,0 +1,75 @@
+package si.deisinger.business.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import si.deisinger.business.configuration.ConfigUtils;
+import si.deisinger.providers.enums.Providers;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Objects;
+import java.util.Properties;
+
+public class EmailController {
+	private static final Logger LOG = LoggerFactory.getLogger(EmailController.class);
+	private static final String USERNAME = ConfigUtils.getEmailUsername();
+	private static final String PASSWORD = ConfigUtils.getEmailPassword();
+	private static final Properties PROPERTIES = createProperties();
+	private static final Session SESSION = createSession();
+
+	/**
+	 * A method that sends an email with the commit URL for the new charging stations for the given provider.
+	 *
+	 * @param provider
+	 * 		the provider
+	 * @param url
+	 * 		the commit URL
+	 */
+	public void sendMail(Providers provider, String url) {
+		LOG.info("Sending email");
+
+		try {
+			Message message = new MimeMessage(SESSION);
+			message.setFrom(new InternetAddress(Objects.requireNonNull(USERNAME)));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(USERNAME));
+			message.setSubject("New charging station for: " + provider.getProviderName());
+			message.setText("Hello there sailor,\nThere are new charging stations from: " + provider.getProviderName() + "\n\n" + url);
+
+			Transport.send(message);
+
+			LOG.info("Email Send");
+
+		} catch (MessagingException e) {
+			LOG.error("Failed to send email: " + e.getMessage(), e);
+		}
+	}
+
+	/**
+	 * This method creates a new Properties object with specific mail settings required for sending email.
+	 *
+	 * @return A Properties object containing the necessary properties for sending an email.
+	 */
+	private static Properties createProperties() {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.host", ConfigUtils.getEmailHost());
+		properties.put("mail.smtp.port", ConfigUtils.getEmailPort());
+		properties.put("mail.smtp.auth", ConfigUtils.getEmailAuth());
+		properties.put("mail.smtp.starttls.enable", ConfigUtils.getEmailTls());
+		return properties;
+	}
+
+	/**
+	 * This method creates a new session for sending email, with a specified Authenticator for handling authentication.
+	 *
+	 * @return A new session object with a specified Authenticator.
+	 */
+	private static Session createSession() {
+		return Session.getInstance(PROPERTIES, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(USERNAME, PASSWORD);
+			}
+		});
+	}
+}
